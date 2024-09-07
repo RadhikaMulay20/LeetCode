99781a5a73c34b5a9d12d15048810a277da42b6c
RadhikaMulay20 <125047909+RadhikaMulay20@users.noreply.github.com>
2024-09-07 22:16:04 +0530

    Time: 18 ms (92.91%) | Memory: 30.7 MB (87.87%) - LeetSync

--- 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/linked-list-in-binary-tree.cpp
@@ -21,22 +21,33 @@
  */
 class Solution {
 public:
-    bool isSubPath(ListNode* head, TreeNode* root) {
-        return dfs(head, head, root);
-    }
-    
-    bool dfs(ListNode* head, ListNode* cur, TreeNode* root) {
-        if (cur == nullptr) return true;  // Successfully matched the list
-        if (root == nullptr) return false; // Reached the end of the tree without matching
-        
-        if (cur->val == root->val) {
-            cur = cur->next;  // Move to the next list node if value matches
-        } else if (head->val == root->val) {
-            head = head->next; // Start new matching attempt if the value matches head of list
-        } else {
-            cur = head;  // Reset the matching pointer
+    bool res = false;
+
+    void check(TreeNode* root, ListNode* head){
+        if(head == NULL){
+            res = true;
+            return;
         }
-        
-        return dfs(head, cur, root->left) || dfs(head, cur, root->right); // Recursively check left and right subtrees
+        if(root == NULL || root->val != head->val){
+            return;
+        }
+
+        check(root->left, head->next);
+        check(root->right, head->next);
+    }
+
+    void dfs(TreeNode* root, ListNode* head){
+        if(root == NULL || head == NULL || res == true) return;
+        if(root->val == head->val){
+            check(root, head);
+        }
+
+        dfs(root->left, head);
+        dfs(root->right, head);
+    }
+
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        dfs(root, head);
+        return res;    
     }
 };
