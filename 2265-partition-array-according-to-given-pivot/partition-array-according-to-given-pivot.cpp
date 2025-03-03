ddccc227009f7723c35e2c87ad5c443afa8ddd65
RadhikaMulay20 <125047909+RadhikaMulay20@users.noreply.github.com>
2025-03-03 22:53:10 +0530

    Time: 0 ms (100.00%) | Memory: 127.7 MB (77.25%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> less, high, result(nums.size());
-        int count = 0;
-
-        for(int num : nums) {
-            if(num < pivot) less.push_back(num);
-            else if(num == pivot) count++;
-            else high.push_back(num);
+        int len=nums.size();//store the length of the array
+        vector<int>res(len);
+        int c=0;
+        int pt=0;
+        for(int i=0;i<len;i++)
+        {
+            if(nums[i]<pivot)
+            {
+                res[pt++]=nums[i];
+                //pt++;
+            }
+            else if(nums[i]==pivot)
+            {
+                c++;// when equal 
+            }
         }
+        while(c--)
+        {
+            res[pt++]=pivot;//equal to pivot
+        }
+        for(int i=0;i<len;i++)
+        {
+            if(nums[i]>pivot)
+            {
+                res[pt]=nums[i];
+                pt++;
 
-        int index = 0;
-        for(int num : less) result[index++] = num;
-        for(int i = 0; i < count; i++) result[index++] = pivot;
-        for(int num : high) result[index++] = num;
+            }
+        }
+        return res;
 
-        return result;
+
+        
     }
 };
