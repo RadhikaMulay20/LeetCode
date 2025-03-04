884d5b4c5679f4745b486360b02d620f25bdb939
RadhikaMulay20 <125047909+RadhikaMulay20@users.noreply.github.com>
2025-03-04 18:51:36 +0530

    Time: 0 ms (100.00%) | Memory: 28.5 MB (53.14%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -2,21 +2,26 @@ class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
 
-        int n=nums.size();
-        sort(nums.begin(), nums.end());
-        int prev=nums[0];
+        // int n=nums.size();
+        // sort(nums.begin(), nums.end());
+        // int prev=nums[0];
 
-        for(int i=1;i<n;i++){
-            int next=nums[i];
-            if(next==prev){
-                return prev;
-            }else{
-                prev=next;
-            }
+        // for(int i=1;i<n;i++){
+        //     int next=nums[i];
+        //     if(next==prev){
+        //         return prev;
+        //     }else{
+        //         prev=next;
+        //     }
 
 
+        // }
+        // return prev;
+
+        for (int i = 0; i + 2 < nums.size(); ++i){
+            if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2])return nums[i];
         }
-        return prev;
+        return nums.back();
         
     }
 };
