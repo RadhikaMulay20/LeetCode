554c81c4d42925d561c44eab451b1099b267ef78
RadhikaMulay20 <125047909+RadhikaMulay20@users.noreply.github.com>
2025-03-05 23:29:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (61.42%) - LeetSync

---
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long coloredCells(int n) {
+
+        // long long min=n-3;
+        // long long ans=13;
+        // if (n==1){
+        //     return 1;
+        // }
+        // else if(n==2) {
+        //     return 5;
+        // }
+        // else if(n==3){
+        //     return 13;
+        // }else{
+        //     while(min<=n){
+
+        //     }
+
+        // }
+        
+        return 2LL*n*(n-1)+1;
+        
+    }
+};
