472eef746884f206d0a81c2d286899423a84e388
RadhikaMulay20 <125047909+RadhikaMulay20@users.noreply.github.com>
2025-03-05 23:42:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (61.42%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,23 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-
-        // long long min=n-3;
-        // long long ans=13;
-        // if (n==1){
-        //     return 1;
-        // }
-        // else if(n==2) {
-        //     return 5;
-        // }
-        // else if(n==3){
-        //     return 13;
-        // }else{
-        //     while(min<=n){
-
-        //     }
-   
-        // }
         
         return (long long)2*n*(n-1)+1;
         
