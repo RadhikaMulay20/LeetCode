ca428d0459f2c1f93459f8f28c6f8c6cc6931f81
RadhikaMulay20 <125047909+RadhikaMulay20@users.noreply.github.com>
2025-03-18 14:10:44 +0530

    Time: 0 ms (100.00%) | Memory: 61.1 MB (28.44%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -1,47 +1,16 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        // Binary search for the longest nice subarray length
-        int left = 0, right = nums.size();
-        int result = 1;  // Minimum answer is 1 (as subarrays of length 1 are
-                         // always nice)
+        int start = 0, end = 0, mask = 0, answer = 1, n = nums.size();
 
-        while (left <= right) {
-            int length = left + (right - left) / 2;
-            if (canFormNiceSubarray(length, nums)) {
-                result = length;    // Update the result
-                left = length + 1;  // Try to find a longer subarray
-            } else {
-                right = length - 1;  // Try a shorter length
+        for(; end < n; end++) {
+            while((mask & nums[end]) != 0) {
+                mask = mask ^ nums[start++];
             }
+            mask |= nums[end];
+            answer = max(answer, end - start + 1);
         }
-        return result;
-    }
 
-private:
-    bool canFormNiceSubarray(int length, vector<int>& nums) {
-        if (length <= 1) return true;  // Subarray of length 1 is always nice
-
-        // Try each possible starting position for subarray of given length
-        for (int start = 0; start <= nums.size() - length; ++start) {
-            int bitMask = 0;  // Tracks the bits used in the current subarray
-            bool isNice = true;
-
-            // Check if the subarray starting at 'start' with 'length' elements
-            // is nice
-            for (int pos = start; pos < start + length; ++pos) {
-                // If current number shares any bits with existing mask,
-                // the subarray is not nice
-                if ((bitMask & nums[pos]) != 0) {
-                    isNice = false;
-                    break;
-                }
-                bitMask |= nums[pos];  // Add current number's bits to the mask
-            }
-
-            if (isNice)
-                return true;  // Found a nice subarray of the specified length
-        }
-        return false;  // No nice subarray of the given length exists
+        return answer;
     }
 };
